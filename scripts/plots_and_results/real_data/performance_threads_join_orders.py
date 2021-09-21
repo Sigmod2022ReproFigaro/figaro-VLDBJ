c94b4e123fb551e16c626f5c31fc6ac4d93a6587
popin1994 <djordjezivanovicva@gmail.com>
2021-09-21 10:52:56 +0200

    1. Added join orders and threading plotting.

--- scripts/plots_and_results/real_data/performance_threads_join_orders.py
+++ scripts/plots_and_results/real_data/performance_threads_join_orders.py
@@ -11,74 +11,68 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 import csv
 import logging
-from data_management.database import Database
-from data_management.database_psql import DatabasePsql
-from data_management.query import Query
 import argparse
 import sys
 import numpy as np
-from evaluation.custom_logging import init_logging
+from evaluation.custom_logging import init_logging, set_logging_level
 import matplotlib.pyplot as plt
 
 
-
-def collect_times(root_path: str, exp_names: list, db_names: list,
+def collect_times(root_path: str, exp_name: str, db_names: list,
         exp_paths: dict, join_orders: dict, thread_nums_exp: dict,
-            start_per: int, end_per: int, per_inc: int,
             num_measurement: int):
     XLSX_NAME = "time.xlsx"
     df_measurement_exp_dbs = {}
-    for exp_name in exp_names:
-        exp_path = exp_paths[exp_name]
-        perf_path = os.path.join(root_path, exp_path)
+    exp_path = exp_paths[exp_name]
+    perf_path = os.path.join(root_path, exp_path)
 
-        for db_name in db_names:
-            gather_times_path = os.path.join(perf_path, db_name + XLSX_NAME)
-            print(gather_times_path)
-            out_workbook = Workbook()
-            out_workbook.remove(out_workbook.active)
-            out_sheet = out_workbook.create_sheet("Times")
-            df_measurement = pd.DataFrame(columns=join_orders[db_name])
+    for db_name in db_names:
+        gather_times_path = os.path.join(perf_path, db_name + XLSX_NAME)
+        print(gather_times_path)
+        out_workbook = Workbook()
+        out_workbook.remove(out_workbook.active)
+        out_sheet = out_workbook.create_sheet("Times")
+        df_measurement = pd.DataFrame(columns=join_orders[db_name])
 
-            for join_idx, join_order in enumerate(join_orders[db_name]):
-                for thread_idx, thread_num in enumerate(thread_nums_exp[exp_name]):
-                    for db_idx, percent in enumerate(range(start_per, end_per + 1, per_inc)):
-                        db_name_full = "{} {} {}".format(db_name, join_order, thread_num)
-                        path_xlsx = os.path.join(perf_path, db_name, join_order+str(thread_num), XLSX_NAME)
-                        if os.path.isfile(path_xlsx):
-                            print("PATH", path_xlsx)
-                            workbook = load_workbook(filename=path_xlsx, data_only=True)
-                            sheet = workbook.active
-                            row_count = sheet.max_row
-                            col_idx = join_idx * thread_nums_exp[exp_name].__len__() + thread_idx + 1
-                            col_letter = get_column_letter(col_idx)
+        for join_idx, join_order in enumerate(join_orders[db_name]):
+            for thread_idx, thread_num in enumerate(thread_nums_exp[exp_name]):
+                db_name_full = "{} {} {}".format(db_name, join_order, thread_num)
+                path_xlsx = os.path.join(perf_path, db_name, join_order+str(thread_num), XLSX_NAME)
+                if os.path.isfile(path_xlsx):
+                    print("PATH", path_xlsx)
+                    workbook = load_workbook(filename=path_xlsx, data_only=True)
+                    sheet = workbook.active
+                    row_count = sheet.max_row
+                    col_idx = join_idx * len(thread_nums_exp[exp_name]) + thread_idx + 1
+                    col_letter = get_column_letter(col_idx)
 
-                            out_sheet.cell(row=1, column=col_idx).value = db_name_full
+                    out_sheet.cell(row=1, column=col_idx).value = db_name_full
 
-                            start_row_idx = 2
-                            np_measures = np.zeros(num_measurement)
-                            for row_idx_dst in range(start_row_idx, start_row_idx + num_measurement):
-                                offset_idx = row_idx_dst - start_row_idx
-                                row_idx_src = row_count - num_measurement  + offset_idx
-                                val = sheet.cell(row=row_idx_src, column=2).value
-                                np_measures[offset_idx] = float(val)
-                                #print(idx_shift, db_idx)
-                                out_sheet.cell(row=row_idx_dst, column=col_idx).value = val
+                    start_row_idx = 2
+                    np_measures = np.zeros(num_measurement)
+                    for row_idx_dst in range(start_row_idx, start_row_idx + num_measurement):
+                        offset_idx = row_idx_dst - start_row_idx
+                        row_idx_src = row_count - num_measurement  + offset_idx
+                        val = sheet.cell(row=row_idx_src, column=2).value
+                        np_measures[offset_idx] = float(val)
+                        out_sheet.cell(row=row_idx_dst, column=col_idx).value = val
 
-                            out_sheet.cell(row=start_row_idx + num_measurement, column=col_idx).value = '=AVERAGE({}{}:{}{})'.format(col_letter, start_row_idx + 1,
-                                    col_letter,6)
-                            time_avg = np.mean(np_measures[1:])
-                        else:
-                            time_avg = float("inf")
-                        df_measurement.at[thread_num, join_order] = time_avg
+                    out_sheet.cell(row=start_row_idx + num_measurement, column=col_idx).value = '=AVERAGE({}{}:{}{})'.format(col_letter, start_row_idx + 1,
+                            col_letter,6)
+                    time_avg = np.mean(np_measures[1:])
+                else:
+                    time_avg = float("inf")
+                df_measurement.at[thread_num, join_order] = time_avg
 
-            out_workbook.save(gather_times_path)
-            df_measurement_exp_dbs[(exp_name, db_name)] = df_measurement.astype(np.float64)
+        out_workbook.save(gather_times_path)
+        df_measurement_exp_dbs[(exp_name, db_name)] = df_measurement.astype(np.float64)
+
+    return df_measurement_exp_dbs
 
 
 def dump_results_to_dat(db_names: list, df_measurement_exps: dict,
     thread_nums_exp: dict):
-    exp_name = "figaro"
+    exp_name = "figaro_thin"
     exp_dat_name = "exp2cores.dat"
     exp_dat_names = ["#cores", "Retailer", "Favorita", "Yelp"]
     dbs_results = []
@@ -96,8 +90,7 @@ def dump_results_to_dat(db_names: list, df_measurement_exps: dict,
     df_db_results.to_csv(exp_dat_name, float_format='%.2f', sep='\t', index=False, quoting=csv.QUOTE_NONE,  escapechar=" ")
 
 
-def plot_performance(db_names: list, exp_names: list,
-    df_measurement_exp_dbs: dict, thread_nums_exp: dict):
+def plot_performance(db_names: list, df_measurement_exp_dbs: dict, thread_nums_exp: dict):
     plt.figure("name", figsize=(16, 8), dpi=80)
     plt.xlabel("Number of threads")
     plt.ylabel("wall-clock-time[s]")
@@ -111,21 +104,17 @@ def plot_performance(db_names: list, exp_names: list,
     #plt.locator_params(axis='y', nbins=20)
     db_marker =  {"DBFavorita": "^", "DBYelp": "s", "DBRetailer": "x"}
     db_colour =  {"DBFavorita": "r", "DBYelp": "c", "DBRetailer": "g"}
-    exp_colour = {"figaro": "r", "mkl": "b"}
 
-    for exp_name in exp_names:
-        if exp_name == "mkl":
-            continue
-        for db_name in db_names:
-            df_measurement = df_measurement_exp_dbs[(exp_name, db_name)]
-            print(df_measurement)
-            min_idx_cols = df_measurement.idxmin(axis="columns")
-            max_num_threads = thread_nums_exp[exp_name][-1]
-            min_join_order = min_idx_cols[max_num_threads]
+    exp_name = "figaro_thin"
+    for db_name in db_names:
+        df_measurement = df_measurement_exp_dbs[(exp_name, db_name)]
+        print(df_measurement)
+        min_idx_cols = df_measurement.idxmin(axis="columns")
+        max_num_threads = thread_nums_exp[exp_name][-1]
+        min_join_order = min_idx_cols[max_num_threads]
 
-            print(min_idx_cols[thread_nums_exp["figaro"][-1]])
-            #for join_order in join_orders[db_name]:
-            plt.plot(df_measurement[min_join_order], "-" + db_colour[db_name] + db_marker[db_name], label="{} {} {}".format(exp_name, db_name, min_join_order), markersize=10)
+        print(min_idx_cols[thread_nums_exp["figaro_thin"][-1]])
+        plt.plot(df_measurement[min_join_order], "-" + db_colour[db_name] + db_marker[db_name], label="{} {} {}".format(exp_name, db_name, min_join_order), markersize=10)
 
     ax = plt.gca()
     def myLogFormat(y,pos):
@@ -148,7 +137,7 @@ def plot_performance(db_names: list, exp_names: list,
     #formatter.set_powerlimits((0,48))
     ax.xaxis.set_major_formatter(formatter)
     plt.legend(loc="upper right")
-    plt.savefig("exp2threads.pdf",bbox_inches='tight')
+    plt.savefig("exp2threads.pdf", bbox_inches='tight')
 
     plt.show()
 
@@ -156,19 +145,19 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--root_path", action="store",
                         dest="root_path", required=True)
-    parser.add_argument("-e", "--exp_names", dest="exp_names", nargs='*', required=True)
+    parser.add_argument("-e", "--exp_name", dest="exp_name",  required=True)
     parser.add_argument("--dump_results", action="store_true",
         dest="dump_results", required=False)
     args = parser.parse_args(args)
 
 
     root_path = args.root_path
-    exp_names = args.exp_names
+    exp_name = args.exp_name
     dump_results = args.dump_results
 
     db_names = ["DBRetailer", "DBFavorita", "DBYelp"]
     figaro_threads = [1, 2, 4, 8, 16, 24, 32, 48]
-    thread_nums_exp = {"mkl": [48], "figaro": figaro_threads}
+    thread_nums_exp = {"mkl": [48], "figaro_thin": figaro_threads}
 
     join_orders = {"DBFavorita": ["HolidaysRoot", "ItemsRoot", "OilRoot", "SalesRoot", "StoresRoot", "TransactionsRoot"],
                 "DBYelp": ["BusinessRoot", "CategoryRoot", "CheckinRoot", "HoursRoot", "ReviewRoot", "UserRoot"],
@@ -176,7 +165,6 @@ def main(args):
 
 
     exp_paths = {"figaro_thin": "comparisons/performance/figaro/thin_diag",
-    "figaro": "comparisons/performance/figaro/thin_diag",
     "mkl": "comparisons/performance/python/mkl",
     "figaro_lapack": "comparisons/performance/figaro/lapack",
     "openblas": "comparisons/performance/python/openblas",
@@ -185,19 +173,16 @@ def main(args):
 
     db_names = ["DBRetailer", "DBFavorita", "DBYelp"]
 
-    start_per = 10
-    end_per = 100
-    per_inc = 10
-
     num_measurement = 5
 
-    df_measurement_exps = collect_times(root_path, exp_names, db_names,
-        exp_paths, join_orders, thread_nums_exp, start_per, end_per, per_inc,
+    df_measurement_exps = collect_times(root_path, exp_name, db_names,
+        exp_paths, join_orders, thread_nums_exp,
         num_measurement)
     if dump_results:
         dump_results_to_dat(db_names, df_measurement_exps, thread_nums_exp)
-    plot_performance(db_names, exp_names, df_measurement_exps,  thread_nums_exp)
+    plot_performance(db_names,  df_measurement_exps,  thread_nums_exp)
 
 if __name__ == "__main__":
     init_logging()
+    set_logging_level(logging.INFO)
     main(sys.argv[1:])
