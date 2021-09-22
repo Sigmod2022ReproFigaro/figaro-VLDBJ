84d76ca3359d004e0a6240bb055b3aab7c00c1d7
popin1994 <djordjezivanovicva@gmail.com>
2021-09-22 02:21:10 +0200

    1. Small fix.

--- scripts/evaluation/experiment.py
+++ scripts/evaluation/experiment.py
@@ -82,8 +82,8 @@ def run_exp1(username: str, password: str, root_path: str,
     args_coll_ohe = args_coll + ["--ohe"]
     args_coll_syn = ["--root_path", root_path, "--dump_results"]
 
-    perf_percent.main(args_coll)
-    #perf_percent.main(args_coll_ohe)
+    #perf_percent.main(args_coll)
+    perf_percent.main(args_coll_ohe)
     #syn_perf.main(args_coll_syn)
 
 
@@ -122,14 +122,16 @@ def run_exp3(username: str, password: str, root_path: str,
     args = ["-p", password, "-u", username, "-r", root_path,
                "-s", system_tests_path]
     args_real_data = args + ["--test", "_real_data"]
-    #args_real_data_ohe = args + ["--test", "_real_data_ohe"]
+    args_real_data_ohe = args + ["--test", "_real_data_ohe"]
 
     #eval.main(args_real_data)
     #eval.main(args_real_data_ohe)
 
     args_coll = ["--root_path", root_path,
         "--exp_name", "figaro_thin", "--dump_results"]
-    perf_join_ord.main(args_coll)
+    args_coll_ohe = args_coll + ["--ohe"]
+    #perf_join_ord.main(args_coll)
+    perf_join_ord.main(args_coll_ohe)
 
 
 def set_exp4_synt(system_tests_path: str):
