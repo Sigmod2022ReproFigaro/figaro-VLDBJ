651351f2d880d17c3ea687666399414020341280
popin1994 <djordjezivanovicva@gmail.com>
2022-04-02 16:13:29 +0200

    1. Further cleaning of datasize computation.

--- scripts/data_management/dataset_size.py
+++ scripts/data_management/dataset_size.py
@@ -2,35 +2,18 @@ import os
 import logging
 import argparse
 import sys
-import data_management.database_full_reducer as dfr
-from evaluation.custom_logging import init_logging
-
-
-
-
-import os
 import pandas as pd
 import numpy as np
 import shutil
-import random
 import json
-
-import sys
-import numpy as np
-import pandas as pd
-from ctypes import CDLL
-from  argparse import ArgumentParser
-from timeit import default_timer as timer
+from evaluation.custom_logging import init_logging
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-import threadpoolctl
-from threadpoolctl import threadpool_limits
 from data_management.database import Database
-
 from data_management.database_psql import DatabasePsql
 from data_management.query import Query
 from evaluation.system_test.system_test import DumpConf
+from sklearn.compose import ColumnTransformer
 
 class DummyEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, sparse):
@@ -52,7 +35,6 @@ class IdentityTransformer(BaseEstimator, TransformerMixin):
         return self
 
 
-
 def dump_qr(dump_file_path, r):
     np.savetxt(dump_file_path, np.asarray(r), delimiter=',')
 
@@ -72,10 +54,9 @@ def transform_data(data, columns, cat_columns, sparse):
     return one_hot_a
 
 
-def ohe_and_dump_database(db_config):
+def ohe_and_dump_database(db_config, query: Query, database: Database):
     db_config_path = db_config["db_config_path"]
     db_ohe_path = db_config["db_ohe_path"]
-    skip_attrs = db_config["skip_attrs"]
     if os.path.exists(db_ohe_path):
         shutil.rmtree(db_ohe_path)
     os.makedirs(db_ohe_path)
@@ -92,20 +73,15 @@ def ohe_and_dump_database(db_config):
         attrs_json = rel_json["attributes"]
         data_path = rel_json["data_path"]
         logging.debug(rel_name)
-        attrs = []
-        attrs_without_dropped = []
-        attrs_cat = []
+        attrs = database.get_all_attribute_names(rel_name=rel_name)
+        attrs_cat = database.get_attribute_names(rel_name=rel_name, is_cat=True)
+        attrs_without_dropped = query.get_join_attrs(rel_name) \
+            +  query.get_non_join_attrs(rel_name)
+        skip_attrs_set = set(query.get_skip_attrs())
         drop_attrs = []
-        for attr_json in attrs_json:
-            attr_name = attr_json["name"]
-            attr_type = attr_json["type"]
-            attrs.append(attr_name)
-            if attr_name in skip_attrs:
-                drop_attrs.append(attr_name)
-            else:
-                attrs_without_dropped.append(attr_name)
-                if attr_type == "category":
-                    attrs_cat.append(attr_name)
+        for attr in attrs:
+            if (attr in skip_attrs_set):
+                drop_attrs.append(attr)
 
         logging.debug("attrs {}".format(attrs))
         logging.debug("cat_attrs {}".format(attrs_cat))
@@ -117,7 +93,9 @@ def ohe_and_dump_database(db_config):
         logging.debug("dropping columns")
         table = table.drop(drop_attrs, axis="columns")
         logging.debug("ohe data")
-        table_np = transform_data(table, attrs_without_dropped, attrs_cat, False)
+        logging.debug("Dimensions before ohe row {} col {}".format(table.shape[0], table.shape[1]))
+        table_np = transform_data(table, attrs_without_dropped, attrs_cat , False)
+        logging.debug("Dimensions after ohe row {} col {}".format(table_np.shape[0], table_np.shape[1]))
         data_out_path = os.path.join(db_ohe_path, rel_name +".csv")
         logging.debug(data_out_path)
         np.savetxt(data_out_path, np.asarray(table_np), delimiter=',')
@@ -125,25 +103,21 @@ def ohe_and_dump_database(db_config):
 
 
 def eval_ohe_and_dump_join(username: str, password: str,
-    db_config):
-    db_config_path = db_config["db_config_path"]
-    query_config_path = db_config["query_config_path"]
-    dump_path = db_config["dump_path"]
-    join_path = dump_path + "join.csv"
-    join_ohe_path = dump_path + "join.csv"
+    database: Database, query: Query, dump_path: str):
+    join_path = os.path.join(dump_path, "join.csv")
+    join_ohe_path = os.path.join(dump_path, "join.csv")
+    logging.debug("join path {}".format(join_path))
 
-    database = Database(db_config_path, "")
-    query = Query(query_config_path=query_config_path, database=database)
     database_psql = DatabasePsql(host_name="",user_name=username,
         password=password, database=database)
 
     database_psql.drop_database()
     database_psql.create_database(database)
 
-    num_repetitions = 1
-    database_psql.evaluate_join(query, num_repetitions=num_repetitions,
+    database_psql.evaluate_join(query, num_repetitions=1,
         order_by=DumpConf.OrderRelation.JOIN_ATTRIBUTE)
     database_psql.dump_join(query, join_path)
+
     # Need due to memory usage.
     database_psql.drop_database()
 
@@ -154,50 +128,43 @@ def eval_ohe_and_dump_join(username: str, password: str,
         query.get_non_join_cat_attr_names_ordered(), False)
     data_out_path = os.path.join(join_ohe_path)
     logging.debug(data_out_path)
+    logging.debug("Dimensions row {} col {}".format(table_np.shape[0], table_np.shape[1]))
     np.savetxt(data_out_path, np.asarray(table_np), delimiter=',')
-    # Get order of attributes to ohe.
+
 
 def ohe_and_dump_join_and_databases(real_dataset_path: str, system_tests_path: str, username: str, password: str):
-    db_names =["retailer", "favorita", "yelp",
+    db_names = ["retailer", "favorita", "yelp",
         "retailer_ohe", "favorita_ohe", "yelp_ohe",
         "retailer", "favorita", "yelp"]
-    db_configs=  {
+    db_configs =  {
         "retailer":
         {
             "db_config_path" : "test_real_data/databases/usretailer/database_specs_usretailer_100.conf",
-            "skip_attrs":  ["Population", "OccupiedHouseUnits", "SubCategory", "CategoryCluster"],
             "query_config_path": "test_real_data/queries/usretailer/query_specs_location_root.conf"
         },
         "favorita":
         {
             "db_config_path" : "test_real_data/databases/favorita/database_specs_favorita_100.conf",
-            "skip_attrs": ["OnPromotion", "HolidayType", "Locale", "LocaleId", "State", "Cluster", "Family", "Perishable"],
             "query_config_path": "test_real_data/queries/favorita/query_specs_stores_root.conf"
         },
         "yelp":
         {
             "db_config_path" : "test_real_data/databases/yelp/database_specs_yelp_100.conf",
-            "skip_attrs": ["ComplimentFunny"],
             "query_config_path": "test_real_data/queries/yelp/query_specs_business_root.conf"
         },
         "retailer_ohe":
         {
             "db_config_path" : "test_real_data_ohe/databases/usretailer/database_specs_usretailer_1_100.conf",
-            "skip_attrs":  [ "Population", "White",  "Asian", "Pacific", "Black",  "MedianAge", "OccupiedHouseUnits", "HouseUnits", "Families", "Households", "HusbWife", "Males", "Females", "HouseholdChildren", "ZipC", "SubCategory", "Category", "CategoryCluster", "LocnC",  "DateIdC", "MaxTemp", "MinTemp", "Thunder"],
             "query_config_path": "test_real_data_ohe/queries/usretailer/query_specs_item_root.conf"
         },
         "favorita_ohe":
         {
             "db_config_path" : "test_real_data_ohe/databases/favorita/database_specs_favorita_1_100.conf",
-            "skip_attrs":  ["OnPromotion", "HolidayType", "Locale", "LocaleId", "State", "Cluster", "Family", "Perishable"],
             "query_config_path": "test_real_data_ohe/queries/favorita/query_specs_items_root.conf"
         },
         "yelp_ohe":
         {
             "db_config_path" : "test_real_data_ohe/databases/yelp/database_specs_yelp_1_100.conf",
-            "skip_attrs":  [
-                    "CityId", "StateId", "Latitude", "Longitude", "StarsB", "ReviewCountB", "IsOpen",
-                    "StarsR", "Useful", "Funny", "Cool","UserIdC", "ReviewIdC","ComplimentFunny"],
             "query_config_path": "test_real_data_ohe/queries/yelp/query_specs_user_root.conf"
         }
     }
@@ -213,8 +180,15 @@ def ohe_and_dump_join_and_databases(real_dataset_path: str, system_tests_path: s
         db_configs[db_name]["query_config_path"] = os.path.join(system_tests_path, db_configs[db_name]["query_config_path"])
 
     for db_name in db_names:
-        #ohe_and_dump_database(db_configs[db_name])
-        eval_ohe_and_dump_join(username, password, db_configs[db_name])
+        db_config = db_configs[db_name]
+        db_config_path = db_config["db_config_path"]
+        query_config_path = db_config["query_config_path"]
+
+        database = Database(db_config_path, "")
+        query = Query(query_config_path=query_config_path, database=database)
+        ohe_and_dump_database(db_configs[db_name], query, database)
+        eval_ohe_and_dump_join(username, password, database,
+            query, db_config["dump_path"])
 
 
 def main(args):
