3f9aa5bff3dcd66803a31a1406dc4d54cd5b62df
popin1994 <djordjezivanovicva@gmail.com>
2022-04-12 19:57:15 +0200

    1. Updating paths in data generation.

--- reproducibility/run_experiments.sh
+++ reproducibility/run_experiments.sh
@@ -7,6 +7,7 @@ FIGARO_SYSTEMS_TESTS_PATH=$FIGARO_CODE_PATH/system_tests
 FIGARO_SCRIPTS_PATH=$FIGARO_CODE_PATH/scripts
 FIGARO_PSQL_USER=zivanovic
 FIGARO_PSQL_PASSWORD=12345
+NUMPY_PATH=/home/zivanovic
 
 ############# Cloning repository #########
 cd $FIGARO_PATH
@@ -17,7 +18,7 @@ python3 -m venv run-env-mkl
 source run-env-mkl/bin/activate
 pip install -r requirements.txt
 pip uninstall -y numpy
-/home/zivanovic/numpy-mkl/setup.py install
+$NUMPY_PATH/numpy-mkl/setup.py install
 deactivate
 
 ######### Setting up python OpenBlas environment ########
@@ -25,11 +26,12 @@ python3 -m venv run-env-openblas
 source run-env-openblas/bin/activate
 pip install -r requirements.txt
 pip uninstall -y numpy
-/home/zivanovic/numpy-openblas/setup.py install
+$NUMPY_PATH/numpy-openblas/setup.py install
 #deactivate
 
 ########## Download data #############
 python -m data_management.data_formating -r $FIGARO_CODE_PATH -d $FIGARO_DATA_PATH -s $FIGARO_SYSTEMS_TESTS_PATH --backup
+python -m evaluation.data_generation -u $FIGARO_PSQL_USER -p $FIGARO_PSQL_PASSWORD  -s $FIGARO_SYSTEMS_TESTS_PATH -d $FIGARO_DATA_PATH --data_type download_real_data
 python -m evaluation.data_generation -u $FIGARO_PSQL_USER -p $FIGARO_PSQL_PASSWORD  -s $FIGARO_SYSTEMS_TESTS_PATH -d $FIGARO_DATA_PATH --data_type all
 # Run experiments
 #for i in {1..4}
